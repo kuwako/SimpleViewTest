e3f76c61181d6da8e6f4e0799e09b28b3386eb48
kuwako <kuwako@leverages.jp>
2016-02-08 12:53:19 +0900

    clickでlabel変更

--- SimpleViewTest/ViewController.swift
+++ SimpleViewTest/ViewController.swift
@@ -9,9 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    @IBAction func unwindowToTop(segue: UIStoryboardSegue) {}
-    
+    @IBOutlet weak var myLabel: UILabel!
+    @IBAction func changeLabel(sender: AnyObject) {
+        myLabel.text = "Hello World"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
